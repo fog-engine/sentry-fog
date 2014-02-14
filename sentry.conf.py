0e45af7d8d3e6d77c77ab9c1a3518b6e4f44d66c
Matthew Schulkind <mschulkind@gmail.com>
2014-02-14 01:09:03 -0500

    Read secret key from the environment.

--- sentry.conf.py
+++ sentry.conf.py
@@ -127,7 +127,7 @@ SERVER_EMAIL = 'root@localhost'
 
 # If this file ever becomes compromised, it's important to regenerate your SECRET_KEY
 # Changing this value will result in all current sessions being invalidated
-SECRET_KEY = 'H6LyXBQtYVyto9qSv947A9ezhst7RlW0JJ5Xfl2l0IJhdegbL4/+Ig=='
+SECRET_KEY = os.environ.get('SENTRY_SECRET_KEY')
 
 # http://twitter.com/apps/new
 # It's important that input a callback URL, even if its useless. We have no idea why, consult Twitter.
